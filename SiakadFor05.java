53249656b5a0c0f793ece57aeec2030acb9dc57a
BintangPancahaya <bintang.panca007@gmail.com>
2024-10-10 08:23:45 +0700

    Modifikasi

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -8,18 +8,27 @@ public class SiakadFor05 {
         Scanner input05 = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidak = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = input05.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
+            } if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidak++;
             }
+            
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidak);
     }
 }
